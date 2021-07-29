f2a2586f9ecc3942c999a975b4b623260827d394
Matheus Aguiar <first_mf@hotmail.com>
2021-07-29 17:14:04 -0300

    finish to-do challenge

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,14 +10,29 @@ export function Home() {
 
   function handleAddTask(newTaskTitle: string) {
     //TODO - add new task
+    const newTask = {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    };
+
+    setTasks((oldState) => [...oldState, newTask]);
   }
 
   function handleToggleTaskDone(id: number) {
     //TODO - toggle task done if exists
+    setTasks((oldState) =>
+      oldState.map((task) => {
+        if (task.id === id) {
+          return { ...task, done: !task.done };
+        } else return task;
+      })
+    );
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
+    setTasks((olsState) => olsState.filter((task) => task.id !== id));
   }
 
   return (
@@ -26,18 +41,18 @@ export function Home() {
 
       <TodoInput addTask={handleAddTask} />
 
-      <TasksList 
-        tasks={tasks} 
+      <TasksList
+        tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
-        removeTask={handleRemoveTask} 
+        removeTask={handleRemoveTask}
       />
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#EBEBEB'
-  }
-})
+    backgroundColor: '#EBEBEB',
+  },
+});
